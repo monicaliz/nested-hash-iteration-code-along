3d2590420f24a16f45fd80c84e366ae71c997fdd
monicaliz <monicaeliz3@gmail.com>
2021-01-24 22:12:01 -0500

    commit tree 3624f943fa661076f7bed750ec65df861a1c5df5

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,6 +1,6 @@
 require 'pry'
 
-  # This is the array we will be passing into the remove_strawberry method
+   # This is the array we will be passing into the remove_strawberry method
   # contacts = {
   #   "Jon Snow" => {
   #     name: "Jon",
@@ -16,4 +16,13 @@ require 'pry'
   
 def remove_strawberry(contacts)
 
+  contacts.each do |person, contact_details_hash|
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if{|ice_cream| ice_cream =="strawberry"}
+        end
+      end
+    end
+  end 
 end
